93f50f06fa1af96346c1c74aefa96c0335cfb438
SystemInfect2 <jdiazdelezana@gmail.com>
2024-11-11 14:26:15 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Si necesitas agregar funcionalidad dinámica, puedes usar este archivo
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Dashboard cargado correctamente.");
+  });
+  
